25ca6a5632889e4fe5daa493a6ed597c4a5409b1
Jose Paquian <alostnight1993@outlook.com>
2020-08-31 18:33:08 -0500

    mvp

--- api/resources/resources-model.js
+++ api/resources/resources-model.js
@@ -8,16 +8,23 @@ const findById = (id) => {
 	return db('resources').where({ id }).first()
 }
 
-const add = (resource) => {
-return db('resources')
-	.insert(resource)
-	.then((ids) => {
-		return findById(ids[0])
-	})
-}
+const add = data => {
+	return db("resources")
+	  .insert(data, "id")
+	  .then(ids => {
+		const [id] = ids;
+  
+		return db("resources")
+		  .where({ id })
+		  .first()
+		  .then(resource => {
+			return resource;
+		  });
+	  });
+  };
 
 module.exports = {
 	find,
 	findById,
-  add
+	add,
 } 
